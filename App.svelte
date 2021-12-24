c1288c473ec86d4986626cf0e40688149a9e4ef3
Tom Taylor <tomptaylor@gmail.com>
2021-12-24 11:54:39 -0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-material-ui@4.0.0/bare.min.css" />
+</svelte:head>
+
+<script>
+	import Button from "@smui/button";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button variant="raised">Hello</Button>
+</main>
---
+++ Button.svelte
@@ -0,0 +1,22 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+	  count += 1;
+	}
+</script>
+
+<style>
+	button {
+	  background: rgba(145, 92, 182);
+	  color: white;
+	  border: none;
+	  padding: 12px 24px;
+	  border-radius: 16px;
+	  box-shadow: 0 0 15px rgba(145, 92, 182, 0.4);
+	}
+</style>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
